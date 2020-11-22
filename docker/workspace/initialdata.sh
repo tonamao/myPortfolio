1e0dc60a3276e198dc5a8c202c3b2e6b23c0c056
tonamao <tonamao@local>
2020-11-22 12:59:04 +0900

    不要な記述削除

--- docker/workspace/initialdata.sh
+++ docker/workspace/initialdata.sh
@@ -1,2 +1,10 @@
-cd /var/www/myPortfolio
-php artisan migrate --seed
+#!/bin/sh
+
+if [ ! -e "/tmp/.initializedfile" ]; then
+    cd /var/www/myPortfolio
+    php artisan migrate --seed
+    touch /tmp/.initializedfile
+    echo "Successfully Initialize MyPortfolio Data."
+else
+    echo "Already Initialized."
+fi
