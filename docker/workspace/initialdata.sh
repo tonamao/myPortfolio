f8972530c0e0ad434233e22f0726da76eaf61cd0
tonamao <tonamao@local>
2020-12-31 21:21:55 +0900

    コンテナ化用ファイル修正

--- docker/workspace/initialdata.sh
+++ docker/workspace/initialdata.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 
+# MySQLサーバーが起動するまでループで待機する
+until mysqladmin ping -h mysql --silent; do
+  echo 'waiting for mysql to be connectable...'
+  sleep 2
+done
+
+echo "Insert init data."
+
 if [ ! -e "/tmp/.initializedfile" ]; then
     cd /var/www/myPortfolio
+    echo "pwd"
+    pwd
+    echo "ls -al"
+    ls -al
     php artisan migrate --seed
     touch /tmp/.initializedfile
     echo "Successfully Initialize MyPortfolio Data."
 else
     echo "Already Initialized."
 fi
+
+/bin/sh
